90202318a8eb4c9484af60d581e5098b6ed7df65
Git Rookie <mail@example.com>
2020-12-16 08:49:51 +0400

    Restore selections and cursor positions

--- vis-commentary.lua
+++ vis-commentary.lua
@@ -44,41 +44,64 @@ local function esc(str)
         :gsub('%?', '%%?'))
 end
 
--- escape '%'
-local function pesc(str)
-    if not str then return "" end
-    return str:gsub('%%', '%%%%')
+local function comment_line(lines, lnum, prefix, suffix, sel)
+    local file = vis.win.file
+    local _, sot_col = lines[lnum]:find('^%s*%S')
+
+    sel:to(lnum, sot_col)
+    local sot_pos = sel.pos
+
+    -- we can't replace text in the line with gsub, we'd risk erasing the mark
+    -- needed to restore initial selection
+    file:insert(sot_pos, prefix .. ' ')
+
+    if suffix ~= '' then
+        local eot_col = lines[lnum]:find('%s*$')
+
+        sel:to(lnum, eot_col)
+        local eot_pos = sel.pos
+
+        file:insert(eot_pos, ' ' .. suffix)
+    end
 end
 
-local function comment_line(lines, lnum, prefix, suffix)
-    if suffix ~= "" then suffix = " " .. suffix end
-    lines[lnum] = string.gsub(lines[lnum],
-                              "(%s*)(.*)",
-                              "%1" .. pesc(prefix) .. " %2" .. pesc(suffix))
-end
+local function uncomment_line(lines, lnum, prefix, suffix, sel)
+    local file = vis.win.file
+    local _, sot_col = lines[lnum]:find('^%s*%S')
+    local pref_len, suff_len = prefix:len() + 1, suffix:len() + 1 -- + spaces
 
-local function uncomment_line(lines, lnum, prefix, suffix)
-    local match_str = "^(%s*)" .. esc(prefix) .. "%s?(.*)" .. esc(suffix)
-    lines[lnum] = table.concat(table.pack(lines[lnum]:match(match_str)))
+    sel:to(lnum, sot_col)
+    local sopref_pos = sel.pos
+
+    file:delete(sopref_pos, pref_len)
+
+    if suffix ~= '' then
+        local sosuff_col = lines[lnum]:find(' ' .. esc(suffix) .. '%s*$') -- start of suffix col
+
+        sel:to(lnum, sosuff_col)
+        local sosuff_pos = sel.pos
+
+        file:delete(sosuff_pos, suff_len)
+    end
 end
 
 local function is_comment(line, prefix)
     return (line:match("^%s*(.+)"):sub(0, #prefix) == prefix)
 end
 
-local function toggle_line_comment(lines, lnum, prefix, suffix)
+local function toggle_line_comment(lines, lnum, prefix, suffix, sel)
     if not lines or not lines[lnum] then return end
     if not lines[lnum]:match("^%s*(.+)") then return end -- ignore empty lines
     if is_comment(lines[lnum], prefix) then
-        uncomment_line(lines, lnum, prefix, suffix)
+        uncomment_line(lines, lnum, prefix, suffix, sel)
     else
-        comment_line(lines, lnum, prefix, suffix)
+        comment_line(lines, lnum, prefix, suffix, sel)
     end
 end
 
 -- if one line inside the block is not a comment, comment the block.
 -- only uncomment, if every single line is comment.
-local function block_comment(lines, a, b, prefix, suffix)
+local function block_comment(lines, a, b, prefix, suffix, sel)
     local uncomment = true
     for i=a,b do
         if lines[i]:match("^%s*(.+)") and not is_comment(lines[i], prefix) then
@@ -89,13 +112,13 @@ local function block_comment(lines, a, b, prefix, suffix)
     if uncomment then
         for i=a,b do
             if lines[i]:match("^%s*(.+)") then
-                uncomment_line(lines, i, prefix, suffix)
+                uncomment_line(lines, i, prefix, suffix, sel)
             end
         end
     else
         for i=a,b do
             if lines[i]:match("^%s*(.+)") then
-                comment_line(lines, i, prefix, suffix)
+                comment_line(lines, i, prefix, suffix, sel)
             end
         end
     end
@@ -103,7 +126,8 @@ end
 
 vis:map(vis.modes.NORMAL, "gcc", function()
     local win = vis.win
-    local lines = win.file.lines
+    local file = win.file
+    local lines = file.lines
     local comment = comment_string[win.syntax]
     if not comment then return end
     local prefix, suffix = comment:match('^([^|]+)|?([^|]*)$')
@@ -111,10 +135,16 @@ vis:map(vis.modes.NORMAL, "gcc", function()
 
     for sel in win:selections_iterator() do
         local lnum = sel.line
-        local col = sel.col
+        local m = file:mark_set(sel.pos)
 
-        toggle_line_comment(lines, lnum, prefix, suffix)
-        sel:to(lnum, col)  -- restore cursor position
+        toggle_line_comment(lines, lnum, prefix, suffix, sel)
+
+        local pos = file:mark_get(m)
+        if pos then -- if the cursor was not on the prefix/suffix when uncommenting
+            sel.pos = pos  -- restore cursor position
+        else
+            sel:to(lnum, 1)
+        end
     end
 
     win:draw()
@@ -123,7 +153,9 @@ end, "Toggle comment on a the current line")
 local function visual_f(i)
     return function()
         local win = vis.win
-        local lines = win.file.lines
+        local file = win.file
+        local lines = file.lines
+        local sel_flip = false
 
         local comment = comment_string[win.syntax]
         if not comment then return end
@@ -134,22 +166,52 @@ local function visual_f(i)
         for sel in win:selections_iterator() do
             local r = sel.range
             local lnum = sel.line     -- line number of cursor
-            local col = sel.col       -- column of cursor
 
             if sel.anchored and r then
+                local cursor_was = 'start'
+                if sel.pos + 1 == r.finish then
+                    cursor_was = 'finish'
+                end
+
                 sel.pos = r.start
                 local a = sel.line
+                local start_m = file:mark_set(sel.pos)
                 sel.pos = r.finish
                 local b = sel.line - i
+                local finish_m = file:mark_set(sel.pos)
 
-                block_comment(lines, a, b, prefix, suffix)
+                block_comment(lines, a, b, prefix, suffix, sel)
 
-                sel:to(lnum, col)     -- restore cursor position
+                r.start = file:mark_get(start_m)
+                r.finish = file:mark_get(finish_m)
+
+                -- if the cursor was not on the prefix/suffix when uncommenting
+                if r.start and r.finish then
+                    sel.range = r -- restore selection
+
+                    local pos = sel.pos
+                    if cursor_was == 'finish' then
+                        pos = sel.pos + 1
+                    end
+                    -- if the cursor is not at the side it was, restore its position
+                    if pos ~= sel.range[cursor_was] then
+                        sel_flip = true
+                    end
+                else
+                    sel:to(lnum, 1)
+
+                    if #win.selections == 1 then
+                        vis.mode = vis.modes.NORMAL -- go to normal mode
+                    end
+                end
             end
         end
 
+        if sel_flip then
+            vis:feedkeys('<vis-selection-flip>')
+        end
+
         win:draw()
-        vis.mode = vis.modes.NORMAL     -- go to normal mode
     end
 end
 
